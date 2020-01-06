1dd6973fb187541f3429ae6f4d55e0f32124aae7
Hunter <thecryptohunter@protonmail.com>
2020-01-06 19:53:16 +0000

    Fix typo

--- install-pivx-staking-node.sh
+++ install-pivx-staking-node.sh
@@ -11,7 +11,7 @@ UNDERLINE='\033[4m'
 function setVars() {
 ## set network dependent variables
 NODE_USER=pivx
-COINCORE=/home/${NODE_USER}/{NODE_USER}/.pivx
+COINCORE=/home/${NODE_USER}/${NODE_USER}/.pivx
 COINPORT=51472 #51474 testnet
 COINRPCPORT=51473 #51475 testnet
 
@@ -22,8 +22,8 @@ RPCUSER=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 RPCPASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 COINBIN=https://github.com/PIVX-Project/PIVX/releases/download/v4.0.0/pivx-4.0.0-x86_64-linux-gnu.tar.gz
 COINDAEMON=${NODE_USER}d
-COINSTARTUP=/home/${NODE_USER}/${NODE_USER}d
-COINSTOP=/home/${NODE_USER}/${NODE_USER}d
+COINSTARTUP=/home/${NODE_USER}/${NODE_USER}-start
+COINSTOP=/home/${NODE_USER}/${NODE_USER}-stop
 COINDLOC=/home/${NODE_USER}/${NODE_USER}node/pivx-4.0.0/bin/
 COINSERVICELOC=/etc/systemd/system/
 COINSERVICENAME=${COINDAEMON}@${NODE_USER}
@@ -251,8 +251,8 @@ cd /home/${NODE_USER}/
     setupSwap
     installFail2Ban
     installFirewall
-    installDependencies
-    compileWallet
+#    installDependencies
+#    compileWallet
     installWallet
     installUnattendedUpgrades
     startWallet
